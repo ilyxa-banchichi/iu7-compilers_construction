2f70d5e1df12b78718b91f699890afbd0f7a8a3d
Илья Петров <ipetrov@gameslab.store>
2025-04-11 05:26:40 +0300

    Добавлена левая факторизация

---
+++ lab2/core/left_factoring.py
@@ -0,0 +1,85 @@
+from collections import defaultdict
+from itertools import groupby
+from typing import List
+import copy
+
+from core.grammar import Grammar
+from core.symbols import *
+
+def left_factoring(grammar: Grammar) -> Grammar:
+    productions = copy.deepcopy(grammar.productions)
+    non_terminals = grammar.non_terminals[:]
+    terminals = grammar.terminals[:]
+
+    prod_map = defaultdict(list)
+    for lhs, rhs in productions:
+        prod_map[lhs].append(rhs)
+
+    changed = True
+
+    while changed:
+        changed = False
+        new_prod_map = defaultdict(list)
+
+        for A in list(prod_map.keys()):
+            grouped = group_prefixes(prod_map[A])
+            if len(grouped) != len(prod_map[A]):
+                changed = True
+
+            for prefix, group in grouped.items():
+                if len(group) == 1:
+                    new_prod_map[A].append(group[0])
+                else:
+                    A_dash = f"{A}'"
+                    non_terminals.append(A_dash)
+                    new_prod_map[A].append(list(prefix) + [A_dash])
+
+                    for rhs in group:
+                        suffix = rhs[len(prefix):] or [EPSILON_CHAR]
+                        new_prod_map[A_dash].append(suffix)
+
+        prod_map = new_prod_map
+
+    final_productions = []
+    for lhs, rhss in prod_map.items():
+        for rhs in rhss:
+            final_productions.append((lhs, rhs))
+
+    return Grammar(
+        non_terminals=non_terminals,
+        terminals=terminals,
+        productions=final_productions,
+        start_symbol=grammar.start_symbol
+    )
+
+
+def group_prefixes(rhs_list: List[List[str]]) -> dict:
+    prefix_groups = defaultdict(list)
+
+    rhs_list.sort()
+    for _, group in groupby(rhs_list, key=lambda x: x[0] if x else ''):
+        group = list(group)
+        if len(group) == 1:
+            prefix_groups[tuple(group[0])].append(group[0])
+        else:
+            prefix = longest_common_prefix(group)
+            prefix_groups[tuple(prefix)].extend(group)
+
+    return dict(prefix_groups)
+
+
+def longest_common_prefix(sequences: List[List[str]]) -> List[str]:
+    if not sequences:
+        return []
+
+    min_len = min(len(seq) for seq in sequences)
+    prefix = []
+
+    for i in range(min_len):
+        current = sequences[0][i]
+        if all(seq[i] == current for seq in sequences):
+            prefix.append(current)
+        else:
+            break
+
+    return prefix
--- lab2/main.py
+++ lab2/main.py
@@ -1,19 +1,23 @@
 from core.grammar import Grammar
 from core.grammar_reader import read_grammar
 from core.eliminating_left_recursion import eliminate_left_recursion
+from core.left_factoring import left_factoring
 
 def process_file(file_name: str):
     grammar = read_grammar("input/" + file_name)
     grammar = eliminate_left_recursion(grammar)
+    grammar = left_factoring(grammar)
     grammar.print()
 
     with open("output/" + file_name, 'w', encoding='utf-8') as file:
         file.write(str(grammar))
 
+
 if __name__ == "__main__":
     files = [
         'example4_7.txt',
         'example4_9.txt',
+        'example4_11.txt',
     ]
     for file_name in files:
         process_file(file_name)
